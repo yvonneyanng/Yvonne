0200c66e3800830a97e624564fe6f6df531fbac3
Yvonne Yang <yvonne@Yvonnes-MacBook-Pro.local>
2023-04-21 04:44:33 +0800

    github test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { styles } from "../styles";
+import "../styles/About.css";
 
 const About = () => {
   return (
-    <div class="text-white text-left bg-sky-900">
-      <h1 class="text-red-500">About me</h1>
+    <div class="text-white text-left bg-sky-900" id="about">
+      <h1 className="about-title">About me</h1>
       <p>I'm a Computer Science student at Feng Chia University.</p>
       <h2>
         Here are some technologies I've been working with:
